f7388f864f1c182645ab73728600f8e38afad114
Valentin Maris <marisvali@yahoo.com>
2024-05-06 18:02:25 +0300

    align code so as to check my refactoring

--- old_spiel.py
+++ old_spiel.py
@@ -33,58 +33,60 @@ def main(_):
     np.random.seed(13)
 
     games_list = pyspiel.registered_games()
-    print("Registered games:")
-    print(games_list)
+    # print("Registered games:")
+    # print(games_list)
 
     action_string = None
 
-    print("Creating game: " + FLAGS.game_string)
-    game = pyspiel.load_game(FLAGS.game_string)
+    for _ in range(0, 100):
+        # print("Creating game: " + FLAGS.game_string)
+        game = pyspiel.load_game(FLAGS.game_string)
 
-    # Create the initial state
-    state = game.new_initial_state()
+        # Create the initial state
+        state = game.new_initial_state()
 
-    # Print the initial state
-    print(str(state))
+        # Print the initial state
+        # print(str(state))
 
-    while not state.is_terminal():
-        # The state can be three different types: chance node,
-        # simultaneous node, or decision node
-        if state.is_chance_node():
-            # Chance node: sample an outcome
-            outcomes = state.chance_outcomes()
-            num_actions = len(outcomes)
-            print("Chance node, got " + str(num_actions) + " outcomes")
-            action_list, prob_list = zip(*outcomes)
-            action = np.random.choice(action_list, p=prob_list)
-            print("Sampled outcome: ",
-                        state.action_to_string(state.current_player(), action))
-            state.apply_action(action)
-        elif state.is_simultaneous_node():
-            # Simultaneous node: sample actions for all players.
-            random_choice = lambda a: np.random.choice(a) if a else [0]
-            chosen_actions = [
-                    random_choice(state.legal_actions(pid))
-                    for pid in range(game.num_players())
-            ]
-            print("Chosen actions: ", [
-                    state.action_to_string(pid, action)
-                    for pid, action in enumerate(chosen_actions)
-            ])
-            state.apply_actions(chosen_actions)
-        else:
-            # Decision node: sample action for the single current player
-            action = random.choice(state.legal_actions(state.current_player()))
-            action_string = state.action_to_string(state.current_player(), action)
-            print("Player ", state.current_player(), ", randomly sampled action: ",
-                        action_string)
-            state.apply_action(action)
-        print(str(state))
+        while not state.is_terminal():
+            # The state can be three different types: chance node,
+            # simultaneous node, or decision node
+            if state.is_chance_node():
+                # Chance node: sample an outcome
+                outcomes = state.chance_outcomes()
+                num_actions = len(outcomes)
+                # print("Chance node, got " + str(num_actions) + " outcomes")
+                action_list, prob_list = zip(*outcomes)
+                action = np.random.choice(action_list, p=prob_list)
+                # print("Sampled outcome: ",
+                #             state.action_to_string(state.current_player(), action))
+                state.apply_action(action)
+            elif state.is_simultaneous_node():
+                # Simultaneous node: sample actions for all players.
+                random_choice = lambda a: np.random.choice(a) if a else [0]
+                chosen_actions = [
+                        random_choice(state.legal_actions(pid))
+                        for pid in range(game.num_players())
+                ]
+                print("Chosen actions: ", [
+                        state.action_to_string(pid, action)
+                        for pid, action in enumerate(chosen_actions)
+                ])
+                state.apply_actions(chosen_actions)
+            else:
+                # Decision node: sample action for the single current player
+                action = np.random.choice(state.legal_actions(state.current_player()))
+                action_string = state.action_to_string(state.current_player(), action)
+                # print("Player ", state.current_player(), ", randomly sampled action: ",
+                #             action_string)
+                state.apply_action(action)
+            # print(str(state))
 
-    # Game is now done. Print utilities for each player
-    returns = state.returns()
-    for pid in range(game.num_players()):
-        print("Utility for player {} is {}".format(pid, returns[pid]))
+        # Game is now done. Print utilities for each player
+        reward = state.returns()[0]
+        # for pid in range(game.num_players()):
+        #     print("Utility for player {} is {}".format(pid, returns[pid]))
+        print(f'{reward}', end=' ')
 
 
 if __name__ == "__main__":
